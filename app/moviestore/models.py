628db199fc514e7a6ac834f3491f1e51745ef128
GeorgiosDolias <gadolias@gmail.com>
2021-11-04 08:33:32 +0200

    Added endpoint for creating users.

--- app/moviestore/models.py
+++ app/moviestore/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
                                         PermissionsMixin
+"""
 from django.conf import settings
-# Create your models here.
+"""
 
 
 class UserManager(BaseUserManager):
